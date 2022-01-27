83bed4feb66740e374cc55955fce9a228332803a
W. Guilherme <ravok123@gmail.com>
2022-01-27 15:44:22 -0300

    [app] remove unused libs

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,8 +3,33 @@ import Conf from 'conf'
 
 const conf = new Conf()
 
+const mockList = [
+  {
+    name: 'test1',
+    done: false,
+    project: 'project 1',
+  },
+  {
+    name: 'test2',
+    done: false,
+    project: 'project 3',
+  },
+  {
+    name: 'test3',
+    done: false,
+    project: 'project 3',
+  },
+]
+
+// const todoList: any = conf.get('todo-list')
+const todoList: any = mockList
 export default function list() {
-  console.log(
-    'works',
-  )
+  if (todoList && todoList.length) {
+    console.log(`Você tem ${todoList.length} tarefas:`)
+
+    todoList.map((item: any) => console.log(`${item.name} - Projeto: ${item.project}`))
+  } else {
+    // user does not have tasks in todoList
+    console.log('Nenhuma tarefa em aberto')
+  }
 }
